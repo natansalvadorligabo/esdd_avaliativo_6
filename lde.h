7704c695387e888b89c2a06257d05d666497a7ea
Giovana Trevizan Barbosa <gi.trevizan.barbosa@gmail.com>
2023-12-14 16:52:10 -0300

    ultimas alteracoes nas assinaturas das funcoes

--- lde.h
+++ lde.h
@@ -3,18 +3,26 @@
 #include <stdbool.h>
 #include "patient.h"
 
+// Estrutura do nó
 typedef struct list* List;
 
+// Estrutura do paciente
 typedef struct patient* Infotype;
 
+// Função que inicializa a lista
 List init_list();
 
+// Função que verifica se a lista está vazia
 bool is_empty_list(List list);
 
+// Função que insere um elemento no final da lista
 bool insert_last(List list, Infotype info);
 
+// Função que insere um elemento em ordem crescente
 bool insert_ascend(List list, Infotype info);
 
+// Função que remove da lista o primeiro elemento
 Infotype delete_first(List list);
 
+// Função que remove da lista o último elemento
 Infotype delete_last(List list);
--- patient.h
+++ patient.h
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdbool.h>  
 
+// Estrutura do paciente
 typedef struct patient* Patient;
 
+// Definição da estrutura do paciente
 struct patient{
     int SUS_number;
     int age;
@@ -11,14 +13,20 @@ struct patient{
     int batch_vaccine;
 };
 
+// Função que inicializa o paciente
 Patient init_patient(int SUS_number, int age, int priority_group);
 
+// Função que retorna o grupo de prioridade do paciente
 int get_priority_group(Patient patient);
 
+// Função que retorna o número do SUS do paciente
 int get_SUS_number(Patient patient);
 
+// Função que retorna o lote da vacina do paciente
 int get_batch_vaccine(Patient patient);
 
+// Função que retorna a idade do paciente
 int get_patient_age(Patient patient);
 
+// Função que altera o lote da vacina do paciente
 void set_batch_vaccine(Patient patient, int batch_vaccine);
